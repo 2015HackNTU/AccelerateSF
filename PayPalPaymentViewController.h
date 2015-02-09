5f2ec5647fac27f65ec0828753d264d12871fd10
PoseidonHoHaiChi <poseidonhohaichi@gmail.com>
2015-02-08 17:15:23 -0800

    Poseidon5

---
+++ PayPalPaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayPalPaymentViewController.h
+//  Linkard
+//
+//  Created by Poseidon Ho on 2/8/15.
+//  Copyright (c) 2015 2015HackNTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayPalPaymentViewController : UIViewController
+
+@end
---
+++ SomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SomeViewController.h
+//  Linkard
+//
+//  Created by Poseidon Ho on 2/8/15.
+//  Copyright (c) 2015 2015HackNTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SomeViewController : UIViewController
+
+@end
